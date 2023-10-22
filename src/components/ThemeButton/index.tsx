0bce127e33f59a1eef669fcec7cd19a198968b00
deepaksy <deepaksy28@gmail.com>
2023-10-22 08:50:42 +0530

    changed theme button

--- src/components/ThemeButton/index.tsx
+++ src/components/ThemeButton/index.tsx
@@ -8,7 +8,7 @@ import dayMode from '../../assets/day-mode.png'
 const ThemeButton = () => {
   const {isDark,toggleTheme} = useTheme()
   return (
-    <img className='theme-button' alt='theme-button' aria-label='theme-button' src={isDark?nightMode:dayMode} onClick={()=>toggleTheme()}/>
+    <img className='theme-button' alt='theme-button' aria-label='theme-button' src={isDark?dayMode:nightMode} onClick={()=>toggleTheme()}/>
   )
 }
 
