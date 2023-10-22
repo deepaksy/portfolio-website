e90f0a63047f0e18acd589c117f79482a004d541
deepaksy <deepaksy28@gmail.com>
2023-10-22 12:04:22 +0530

    update

--- src/components/BottonNavbar/index.tsx
+++ src/components/BottonNavbar/index.tsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom'
 import './index.css'
+
+const navLinks = [
+  {name:'Home',path:'/'},
+  {name:'About',path:'/about'},
+  {name:'Feedback',path:'/feedback'},
+]
 
 const ButtonNavbar = () => {
   return (
     <nav className='bottom-navbar'>
-        i am bottom navbar
+        {navLinks.map(({name,path},i)=>{
+          return(
+            <Link to={path}> <button className='bottom-navbar-button' key={i}>{name}</button></Link>
+          )
+        })}
     </nav>
   )
 }
