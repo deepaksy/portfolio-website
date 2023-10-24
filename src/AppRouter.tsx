53a0e05d8fe435281729da74a9165a241e07695c
deepaksy <deepaksy28@gmail.com>
2023-10-24 13:22:14 +0530

    update

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -8,7 +8,8 @@ import App from './App'
 import Navbar from './components/Navbar'
 import About from './pages/About'
 import Feedback from './pages/Feedback'
-import ButtonNavbar from './components/BottonNavbar'
+import ButtonNavbar from './components/BottomNavbar'
+import Test from './pages/Test'
 
 const AppRouter = () => {
   return (
@@ -18,6 +19,7 @@ const AppRouter = () => {
             <Route path='/' element={<App/>}/>
             <Route path='/about' element={<About/>}/>
             <Route path='/feedback' element={<Feedback/>}/>
+            <Route path='/test' element={<Test/>} />
         </Routes>
         <ButtonNavbar/>
     </Router>
