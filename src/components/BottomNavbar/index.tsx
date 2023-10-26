784af36aa19ab1674cd6c90a877ba31930fcc0f2
deepaksy <deepaksy28@gmail.com>
2023-10-26 23:22:14 +0530

    initial commit

--- src/components/BottomNavbar/index.tsx
+++ src/components/BottomNavbar/index.tsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom'
 import './index.css'
 import {Link as ScrollLink} from 'react-scroll'
+import { useState } from 'react'
+import { bottomNavbarSectionType, bottomNavbarSections } from '../../data/section'
 
-const navLinks = [
-  {name:'Home',path:'/'},
-  {name:'About',path:'/about'},
-  {name:'Feedback',path:'/feedback'},
-]
+
 
 const ButtonNavbar = () => {
+  
+  const [activeSection,setActiveSection] = useState<bottomNavbarSectionType>(bottomNavbarSections[0])
   return (
     <nav className='bottom-navbar'>
-        {navLinks.map(({name,path},i)=>{
+        {bottomNavbarSections.map(({title,link},i)=>{
           return(
-            <Link key={name} to={path}> <button className='bottom-navbar-button' key={i}>{name}</button></Link>
+            <Link onClick={()=>setActiveSection({title,link})}  key={title} to={link}> <button className={`bottom-navbar-button ${title===activeSection.title?'active':''}`} key={i}>{title}</button></Link>
           )
         })}
         <ScrollLink to='abc' spy smooth offset={100} duration={500}><button className='bottom-navbar-button'>Div</button></ScrollLink>
---
+++ src/data/section.ts
@@ -0,0 +1,10 @@
+export type bottomNavbarSectionType = {
+    title:string,
+    link:string
+}
+
+export const bottomNavbarSections:bottomNavbarSectionType[] = [
+    {title:'Home',link:'/'},
+    {title:'About',link:'/about'},
+    {title:'Feedback',link:'/feedback'}
+]
